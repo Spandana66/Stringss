f314d1a9437873e33be7eaac8ce23d9501d5b3a7
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-29 12:17:25 +0530

    Create Check_String.java

---
+++ Check_String.java
@@ -0,0 +1,12 @@
+package Stringssss;
+
+public class Check_String 
+{
+public static void main(String[] args)
+{
+	String s= "Hello";
+	String s1=new String("Hello");
+	System.out.println(s==s1);
+	System.out.println(s.equals(s1));
+}
+}
