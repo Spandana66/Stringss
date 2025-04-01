872d5d4b0c300c69e1e407009b05241051b77c8c
Spandana66 <macharalaspandana1999@gmail.com>
2025-04-01 13:25:32 +0530

    Create Find_Length.java

---
+++ Find_Length.java
@@ -0,0 +1,14 @@
+package Stringssss;
+import java.util.*;
+public class Find_Length
+{
+public static void main(String[] args) 
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the String:");
+String s= sc.next();
+int size=s.length();
+System.out.println("The size of stringis:"+size);
+
+}
+}
