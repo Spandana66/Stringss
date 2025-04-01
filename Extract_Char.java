0c25dbe7914ebc8076c4e53d04b491b372dcaf11
Spandana66 <macharalaspandana1999@gmail.com>
2025-04-01 13:24:37 +0530

    Create Extract_Char.java

---
+++ Extract_Char.java
@@ -0,0 +1,14 @@
+package Stringssss;
+import java.util.*;
+
+public class Extract_Char 
+{
+public static void main(String[] args) 
+{
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter the String:");
+	String s=sc.next();
+	char ch=s.charAt(3);
+	System.out.println(ch);
+}
+}
