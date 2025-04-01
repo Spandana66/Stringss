03cacef0d77f78dc8dbf55ab85a47e68aa58fb27
Spandana66 <macharalaspandana1999@gmail.com>
2025-04-01 13:26:17 +0530

    Create Length_char.java

---
+++ Length_char.java
@@ -0,0 +1,13 @@
+package Stringssss;
+import java.util.*;
+public class Length_Char
+{
+public static void main(String[] args) 
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the String:");
+	String s=sc.next();
+	int l=s.length();
+	System.out.println(l);
+}
+}
